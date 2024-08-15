0260ac697d4063fd0f6f1d39e6442958a556cfde
wintmain <wosintmain@gmail.com>
2024-08-15 22:39:39 +0800

    [basic] Temp resolve lint error

--- app-catalog/samples/basic/src/main/java/com/wintmain/basic/prejob/upDemo/ep9_4.java
+++ app-catalog/samples/basic/src/main/java/com/wintmain/basic/prejob/upDemo/ep9_4.java
@@ -17,13 +17,16 @@
 package com.wintmain.basic.prejob.upDemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 public class ep9_4 extends Activity {
     private Receiver1 receiver1;
@@ -31,6 +34,7 @@ public class ep9_4 extends Activity {
     private Receiver3 receiver3;
     private Receiver4 receiver4;
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,24 +57,24 @@ public class ep9_4 extends Activity {
         IntentFilter intentFilter1 = new IntentFilter();
         intentFilter1.addAction("com.test.broadcasttest.My_TestBroadcast");
         intentFilter1.setPriority(88);
-        registerReceiver(receiver1, intentFilter1);
+        registerReceiver(receiver1, intentFilter1, Context.RECEIVER_NOT_EXPORTED);
 
         // 优先级比1高，则会先响应
         receiver2 = new Receiver2();
         IntentFilter intentFilter2 = new IntentFilter();
         intentFilter2.addAction("com.test.broadcasttest.My_TestBroadcast");
         intentFilter2.setPriority(100);
-        registerReceiver(receiver2, intentFilter2);
+        registerReceiver(receiver2, intentFilter2, Context.RECEIVER_NOT_EXPORTED);
 
         // 3接收到后会截断，导致4接收不到
         receiver3 = new Receiver3();
         IntentFilter intentFilter3 = new IntentFilter();
         intentFilter3.addAction("com.test.broadcasttest.My_TestBroadcast");
-        registerReceiver(receiver3, intentFilter3);
+        registerReceiver(receiver3, intentFilter3, Context.RECEIVER_NOT_EXPORTED);
 
         receiver4 = new Receiver4();
         IntentFilter intentFilter4 = new IntentFilter();
         intentFilter4.addAction("com.test.broadcasttest.My_TestBroadcast");
-        registerReceiver(receiver4, intentFilter4);
+        registerReceiver(receiver4, intentFilter4, Context.RECEIVER_NOT_EXPORTED);
     }
 }
