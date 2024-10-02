f005db0d1fda877a81a29516d49cc0398dbabf30
wintmain <wosintmain@gmail.com>
2024-10-03 00:01:17 +0800

    [foundation] Refactor module name

---
+++ app-catalog/samples/foundation/src/main/java/com/wintmain/foundation/prejob/extDemo/ep5_10.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023-2024 wintmain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wintmain.foundation.prejob.extDemo;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.wintmain.foundation.R;
+
+public class ep5_10 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        EditText editText = new EditText(this);
+        editText.setText(R.string.edittext);
+        linearLayout.addView(editText);
+        registerForContextMenu(editText); // 为编辑框注册上下文菜单
+    }
+
+    @Override
+    public void onCreateContextMenu(
+            ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.contextmenu2, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
---
+++ app-catalog/samples/foundation/src/main/java/com/wintmain/foundation/prejob/upDemo/ep9_3.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023-2024 wintmain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wintmain.foundation.prejob.upDemo;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import com.wintmain.foundation.R;
+
+public class ep9_3 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ep9_3);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent1 = new Intent();
+                        intent1.setAction("com.android.MY_BROADCAST");
+                        intent1.setComponent(
+                                new ComponentName(
+                                        "com.wintmain.up_demo",
+                                        "com.wintmain.up_demo.MyBroadcastReceiver"));
+                        sendBroadcast(intent1);
+                    }
+                });
+        button1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent2 = new Intent();
+                        intent2.setAction("com.android.brocasttest.MY_BROADCAST");
+                        intent2.setComponent(
+                                new ComponentName(
+                                        "com.wintmain.up_demo",
+                                        "com.wintmain.up_demo.AnotherBroadcastReceiver"));
+                        sendBroadcast(intent2);
+                    }
+                });
+    }
+}
